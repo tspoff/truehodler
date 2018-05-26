996c43e3ad864d7d3aca276ac03648dd777d97db
tspoff <tspoff@gmail.com>
2018-05-25 23:03:41 -0500

    truffle deploy fix

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,8 +3,16 @@ const GeneScienceTest = artifacts.require('./GeneScienceTest.sol');
 const SaleClockAuction = artifacts.require('./SaleClockAuction.sol');
 
 module.exports = async function (deployer) {
-  await deployer.deploy(CoinCore);
-  const coinInstance = await CoinCore.deployed();
-  await deployer.deploy(GeneScienceTest);
-  await deployer.deploy(SaleClockAuction, coinInstance.address, 10);
+
+  let coinInstance;
+  deployer.deploy(CoinCore).then(function () {
+    return CoinCore.deployed().then(function (deployed) {
+      coinInstance = deployed;
+      return deployer.deploy(GeneScienceTest).then(function () {
+        return deployer.deploy(SaleClockAuction, coinInstance.address, 10).then(function () {
+          //do something else
+        })
+      })
+    })
+  })
 }
