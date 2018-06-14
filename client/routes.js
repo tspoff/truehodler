693cbc426a9bbe015a52c84a1a93bc92e5227755
tspoff <tspoff@gmail.com>
2018-06-13 22:33:10 -0500

    remove routes.js

--- client/routes.js
+++
@@ -1,9 +0,0 @@
-const routes = require('next-routes')();
-
-routes
-    .add('/', '/index')
-    .add('/admin', '/admin')
-    .add('/coin/:coinId', '/coin/show')
-    .add('/profile/:address', '/profile/show');
-
-module.exports = routes;
